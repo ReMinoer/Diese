58ef5894c29e73bc2dc8542e0c57a52e8c2d1396
ReMinoer <rmorillon@freneticexperience.com>
2015-02-14 20:48:07 +0100

    Forgotten log sample project

---
+++ Samples/Diese.Sample.Log/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Forms;
+using Diese.Debug;
+
+namespace Diese.Sample.Log
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Debug.Log.Message("Run program.exe...", DefaultLogTag.System);
+            Debug.Log.Message("I'm a system message.", DefaultLogTag.System);
+            Debug.Log.Message("I'm a warning.", DefaultLogTag.Warning);
+            Debug.Log.Message("I'm an error.", DefaultLogTag.Error);
+            Debug.Log.Message("I'm a debug message.", DefaultLogTag.Debug);
+
+            Debug.Log.Enable = false;
+            Debug.Log.Message("I'm hide.", DefaultLogTag.Debug);
+            Debug.Log.Enable = true;
+
+            Debug.Log.UseExecutionTime = true;
+            Debug.Log.Message("I use the execution time.", DefaultLogTag.Debug);
+            Debug.Log.UseExecutionTime = false;
+            Debug.Log.Message("I use the real time.", DefaultLogTag.Debug);
+
+            Debug.Log.Message("I'm a normal message.");
+        }
+    }
+}
---
+++ Samples/Diese.Sample.Log/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Diese.Sample.Log
+{
+    static internal class Program
+    {
+        /// <summary>
+        ///     Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static private void Main()
+        {
+            Debug.Log.Instantiate(true);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
