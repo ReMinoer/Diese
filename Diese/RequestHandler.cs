2d80c04a484a10cf001d570219015cf0c4dbd47c
ReMinoer <rmorillon@freneticexperience.com>
2015-07-23 22:32:05 +0200

    Add RequestHandler

---
+++ Diese/RequestHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Diese
+{
+    public class RequestHandler<TRequestArgs, TFinalizeArgs>
+        where TRequestArgs : EventArgs
+    {
+        public EventHandler<TFinalizeArgs> Finalize { get; set; }
+        public event EventHandler<TRequestArgs> Event;
+
+        public RequestHandler(EventHandler<TFinalizeArgs> finalize)
+        {
+            Finalize = finalize;
+        }
+
+        public void Invoke(object sender, TRequestArgs requestArgs)
+        {
+            if (Event != null)
+                Event(sender, requestArgs);
+        }
+    }
+}
