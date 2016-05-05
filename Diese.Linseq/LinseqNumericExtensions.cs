8976c5fc82fb1448e175e3943fc1555a1e544828
Rémi Morillon <rmorillon@freneticexperience.com>
2016-05-05 18:14:23 +0200

    Add Diese.Linseq to handle sequences

---
+++ Diese.Linseq/LinseqNumericExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Diese.Linseq
+{
+    static public class LinseqNumericExtensions
+    {
+        static public IEnumerable<int> ThenGoTo(this IEnumerable<int> sequence, int goal, int step = 1)
+        {
+            return sequence.ThenGoTo(goal, current => goal.CompareTo(current) * step);
+        }
+
+        static public IEnumerable<double> ThenGoTo(this IEnumerable<double> sequence, double goal, double step = 1)
+        {
+            return sequence.ThenGoTo(goal, current => goal.CompareTo(current) * step);
+        }
+
+        static public IEnumerable<float> ThenGoTo(this IEnumerable<float> sequence, float goal, float step = 1)
+        {
+            return sequence.ThenGoTo(goal, current => goal.CompareTo(current) * step);
+        }
+    }
+}
---
+++ Diese.Linseq/Sequence.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Diese.Linseq
+{
+    static public class Sequence
+    {
+        static public IEnumerable<T> StartWith<T>(params T[] values)
+        {
+            return Enumerable.Empty<T>().ThenAdd(values);
+        }
+    }
+}
