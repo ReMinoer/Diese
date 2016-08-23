3bcf2e90ff1775bb03279486772a949b25348cea
Rémi Morillon <rmorillon@freneticexperience.com>
2016-08-23 22:26:08 +0200

    Add Defaultable

---
+++ Diese/Defaultable.cs
@@ -0,0 +1,34 @@
+﻿namespace Diese
+{
+    public class Defaultable<T>
+    {
+        private bool _userDefined;
+        private T _userValue;
+        public T DefaultValue { get; set; }
+
+        public T Value
+        {
+            get { return _userDefined ? _userValue : DefaultValue; }
+            set
+            {
+                _userValue = value;
+                _userDefined = true;
+            }
+        }
+
+        public Defaultable()
+        {
+        }
+
+        public Defaultable(T defaultValue)
+        {
+            DefaultValue = defaultValue;
+        }
+
+        public void SetToDefault()
+        {
+            _userValue = default(T);
+            _userDefined = false;
+        }
+    }
+}
