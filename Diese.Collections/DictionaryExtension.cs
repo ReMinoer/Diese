884b693e74839b55e4b973d8bb230cf641979876
Rémi Morillon <ReMinoer@users.noreply.github.com>
2016-11-30 16:11:21 +0100

    Add Any Dictionary extension

--- Diese.Collections/DictionaryExtension.cs
+++ Diese.Collections/DictionaryExtension.cs
@@ -5,6 +5,21 @@ namespace Diese.Collections
 {
     static public class DictionaryExtension
     {
+        static public bool Any<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, Predicate<TKey> predicate, out TValue item)
+        {
+            foreach (KeyValuePair<TKey, TValue> obj in dictionary)
+            {
+                if (!predicate(obj.Key))
+                    continue;
+
+                item = obj.Value;
+                return true;
+            }
+
+            item = default(TValue);
+            return false;
+        }
+        
         static public void AddRange<T, TKey, TValue>(this IDictionary<TKey, TValue> dictionary, IEnumerable<T> enumerable, Func<T, TKey> keySelector, Func<T, TValue> valueSelector)
         {
             foreach (T item in enumerable)
