2c7437bb81ee6699a9364a1f82d3b7e92c4a8686
Rémi Morillon <rmorillon@freneticexperience.com>
2017-08-15 14:48:08 +0200

    Add expression extension for property delegates

---
+++ Diese/ExpressionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Diese
+{
+    static public class ExpressionExtensions
+    {
+        static public Func<TProperty> Getter<TProperty>(this Expression<Func<TProperty>> propertyLambda)
+        {
+            return Expression.Lambda<Func<TProperty>>(propertyLambda.Body).Compile();
+        }
+
+        static public Action<TObject, TProperty> Setter<TObject, TProperty>(this Expression<Func<TObject, TProperty>> propertyLambda)
+        {
+            var member = (MemberExpression)propertyLambda.Body;
+            ParameterExpression param = Expression.Parameter(typeof(TProperty), "value");
+            return Expression.Lambda<Action<TObject, TProperty>>(Expression.Assign(member, param), propertyLambda.Parameters[0], param).Compile();
+        }
+    }
+}
