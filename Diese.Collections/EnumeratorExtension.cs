ad985f37e0f9a2a567679224724367fe4d415bed
Rémi Morillon <rmorillon@freneticexperience.com>
2017-10-21 00:46:15 +0200

    Add Enumerator extension AsEnumerable

---
+++ Diese.Collections/EnumeratorExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Diese.Collections
+{
+    static public class EnumeratorExtension
+    {
+        static public IEnumerable<T> AsEnumerable<T>(this IEnumerator<T> enumerator)
+        {
+            while (enumerator.MoveNext())
+                yield return enumerator.Current;
+        }
+    }
+}
