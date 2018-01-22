7dba5e31c97a4bbdeb3c94886d4d495fe7bf10f5
Rémi Morillon <rmorillon@freneticexperience.com>
2018-01-22 20:49:53 +0100

    Add Inverse extension method for Comparison

---
+++ Diese/ComparisonExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Diese
+{
+    static public class ComparisonExtension
+    {
+        static public Comparison<T> Inverse<T>(this Comparison<T> comparison)
+        {
+            return (x, y) => comparison(x, y) * -1;
+        }
+
+        static public Func<T, T, int> Inverse<T>(this Func<T, T, int> comparison)
+        {
+            return (x, y) => comparison(x, y) * -1;
+        }
+    }
+}
