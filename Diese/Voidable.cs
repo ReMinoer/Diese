f5f4f7ccb7e01a5ea32b722e75c6633645085c3f
ReMinoer <rmorillon@freneticexperience.com>
2019-07-07 21:38:26 +0200

    Add Voidable struct and unit tests

---
+++ Diese/Voidable.cs
@@ -0,0 +1,18 @@
+﻿namespace Diese
+{
+    public struct Voidable<T>
+    {
+        static public Voidable<T> Void => new Voidable<T>();
+
+        public bool HasValue { get; }
+        public T Value { get; }
+
+        public Voidable(T value)
+        {
+            Value = value;
+            HasValue = true;
+        }
+
+        static public implicit operator Voidable<T>(T value) => new Voidable<T>(value);
+    }
+}
---
+++ Tests/Diese.Test/VoidableTest.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Diese.Test
+{
+    public class VoidableTest
+    {
+        [Test]
+        static public void DefaultConstructorTest()
+        {
+            var voidable = new Voidable<object>();
+
+            voidable.Value.Should().BeNull();
+            voidable.HasValue.Should().BeFalse();
+        }
+
+        [Test]
+        static public void ConstructorWithValueTest()
+        {
+            var value = new object();
+            var voidable = new Voidable<object>(value);
+
+            voidable.Value.Should().Be(value);
+            voidable.HasValue.Should().BeTrue();
+        }
+
+        [Test]
+        static public void ConstructorWithNullTest()
+        {
+            var voidable = new Voidable<object>(null);
+            
+            voidable.Value.Should().BeNull();
+            voidable.HasValue.Should().BeTrue();
+        }
+
+        [Test]
+        static public void StaticVoidTest()
+        {
+            Voidable<object> voidable = Voidable<object>.Void;
+
+            voidable.Value.Should().BeNull();
+            voidable.HasValue.Should().BeFalse();
+        }
+
+        [Test]
+        static public void ImplicitOperatorWithValueTest()
+        {
+            var value = new object();
+            Voidable<object> voidable = value;
+
+            voidable.Value.Should().Be(value);
+            voidable.HasValue.Should().BeTrue();
+        }
+
+        [Test]
+        static public void ImplicitOperatorWithNullTest()
+        {
+            Voidable<object> voidable = null;
+
+            voidable.Value.Should().BeNull();
+            voidable.HasValue.Should().BeTrue();
+        }
+    }
+}
