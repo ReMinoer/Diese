cc72fb55f4d45d2d379af82e33b5302b1de11b23
ReMinoer <rmorillon@freneticexperience.com>
2019-07-13 12:00:29 +0200

    Add NotifyPropertyChangedBase class

---
+++ Diese/NotifyPropertyChangedBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Diese
+{
+    public class NotifyPropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(value, field))
+                return;
+
+            field = value;
+            NotifyPropertyChanged(propertyName);
+        }
+
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
