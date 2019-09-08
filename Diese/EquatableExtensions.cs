69e333ac11d396933c3dfc77e6b78fc52183cd5a
ReMinoer <rmorillon@freneticexperience.com>
2019-09-08 10:17:06 +0200

    Add NullableEquals extension

---
+++ Diese/EquatableExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Diese
+{
+    static public class EquatableExtensions
+    {
+        static public bool NullableEquals<T>(this IEquatable<T> value, IEquatable<T> other)
+        {
+            return value?.Equals(other) ?? other == null;
+        }
+    }
+}
