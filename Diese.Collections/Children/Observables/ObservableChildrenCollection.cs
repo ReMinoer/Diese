e5028fdd7f910ea6b7989be41bc4eb014bb354cb
ReMinoer <rmorillon@freneticexperience.com>
2019-09-08 10:16:12 +0200

    Implements children collection handling parenting

---
+++ Diese.Collections/Children/Observables/ObservableChildrenCollection.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using Diese.Collections.Observables;
+using Diese.Collections.Utils;
+
+namespace Diese.Collections.Children.Observables
+{
+    public class ObservableChildrenCollection<TOwner, TChildren> : ObservableChildrenCollection<TOwner, TChildren, IObservableCollection<TChildren>>
+        where TOwner : class
+        where TChildren : class, IParentable<TOwner>
+    {
+        public ObservableChildrenCollection(TOwner owner)
+            : base(owner, new ObservableCollection<TChildren>())
+        {
+        }
+    }
+
+    public class ObservableChildrenCollection<TOwner, TChildren, TCollection> : ChildrenCollection<TOwner, TChildren, TCollection>, IObservableCollection<TChildren>, IDisposable
+        where TOwner : class
+        where TChildren : class, IParentable<TOwner>
+        where TCollection : class, IObservableCollection<TChildren>
+    {
+        private readonly CollectionChangeDelayer<TChildren> _collectionChangeDelayer;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        protected ObservableChildrenCollection(TOwner owner, TCollection collection)
+            : base(owner, collection)
+        {
+            _collectionChangeDelayer = new CollectionChangeDelayer<TChildren>(collection);
+        }
+
+        protected override bool CheckAndAdd(TChildren item)
+        {
+            if (!base.CheckAndAdd(item))
+                return false;
+            
+            NotifyCollectionChange();
+            return true;
+        }
+
+        public override bool Remove(TChildren item)
+        {
+            if (!base.Remove(item))
+                return false;
+            
+            NotifyCollectionChange();
+            return true;
+        }
+
+        public override void Clear()
+        {
+            if (Count == 0)
+                return;
+
+            base.Clear();
+            NotifyCollectionChange();
+        }
+
+        protected void NotifyCollectionChange()
+        {
+            if (_collectionChangeDelayer.GetCountChange())
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Count)));
+
+            CollectionChanged?.Invoke(this, _collectionChangeDelayer.GetLastChange());
+        }
+
+        public void Dispose()
+        {
+            _collectionChangeDelayer.Dispose();
+        }
+    }
+}
---
+++ Diese.Collections/Utils/CollectionChangeDelayer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Specialized;
+using Diese.Collections.Observables;
+
+namespace Diese.Collections.Utils
+{
+    public class CollectionChangeDelayer<T> : IDisposable
+    {
+        private readonly IObservableCollection<T> _collection;
+        private NotifyCollectionChangedEventArgs _lastChange;
+
+        public CollectionChangeDelayer(IObservableCollection<T> collection)
+        {
+            _collection = collection;
+            _collection.CollectionChanged += OnCollectionChanged;
+        }
+
+        private void OnCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (_lastChange != null)
+                throw new InvalidOperationException();
+
+            _lastChange = e;
+        }
+
+        public NotifyCollectionChangedEventArgs GetLastChange()
+        {
+            if (_lastChange == null)
+                throw new InvalidOperationException();
+
+            NotifyCollectionChangedEventArgs result = _lastChange;
+            _lastChange = null;
+            return result;
+        }
+
+        public bool GetCountChange() => (_lastChange ?? throw new InvalidOperationException()).Action != NotifyCollectionChangedAction.Replace;
+
+        public void Dispose()
+        {
+            _collection.CollectionChanged -= OnCollectionChanged;
+        }
+    }
+}
